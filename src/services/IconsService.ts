535028f93d7b87cdd09d5ac28317ed159126aae6
Lance Turbes <lturbes@mahalobanking.com>
2023-06-13 23:05:31 -0700

    refactor: rm needless class

--- src/services/IconsService.ts
+++ src/services/IconsService.ts
@@ -1,23 +1,21 @@
 import { IconListEntry } from "@/types";
 import { API_URL, ASSETS_DIR } from "@/config";
 
-class IconsService {
-  public static async getIconList() {
-    const iconList: IconListEntry[] = [];
-    for await (const icon of Deno.readDir(`${ASSETS_DIR}/icons`)) {
-      iconList.push({
-        name: icon.name.split(".")[0],
-        url: `${API_URL}/icons/${icon.name.split(".")[0]}`,
-      });
-    }
-    return iconList;
+const getIconList = async () => {
+  const iconList: IconListEntry[] = [];
+  for await (const icon of Deno.readDir(`${ASSETS_DIR}/icons`)) {
+    iconList.push({
+      name: icon.name.split(".")[0],
+      url: `${API_URL}/icons/${icon.name.split(".")[0]}`,
+    });
   }
+  return iconList;
+};
 
-  public static async getIconByName(iconName: string) {
-    const filepath = `${ASSETS_DIR}/icons/${iconName}.svg`;
-    const file = await Deno.open(filepath, { read: true });
-    return file;
-  }
-}
+const getIconByName = async (iconName: string) => {
+  const filepath = `${ASSETS_DIR}/icons/${iconName}.svg`;
+  const file = await Deno.open(filepath, { read: true });
+  return file;
+};
 
-export default IconsService;
+export default { getIconList, getIconByName };
